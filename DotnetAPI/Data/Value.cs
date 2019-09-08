94d33ddc4b4ec8d33a310145affdb9e2e53e0237
Donald Kibet <chelashawdonald@yahoo.com>
2019-09-08 21:19:50 +0300

    Fix docker script for Sqlite

--- DotnetAPI/Data/Value.cs
+++ DotnetAPI/Data/Value.cs
@@ -1,8 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace dotnet_app.Data
 {
     public class Value
     {
+        
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
     }
 }
